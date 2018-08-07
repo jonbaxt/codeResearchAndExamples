319a971bc379420acfd6b487bd57e8354439c242
jonbaxt <jonbaxt@gmail.com>
2018-08-06 22:56:08 -0600

    Added extra notes and started the Toy Problems

---
+++ Toy Problems/0 Steps to Solving a Problem.js
@@ -0,0 +1,17 @@
+// Most Toy Problems questions initially taken from the repo from DevMountain JS Toy Problems repo: https://github.com/DevMountain/JS-Toy-Problems/
+
+// Step 1: Clarify - Understand the question
+    
+// Step 2: What data structures are needed
+
+// Step 3: Create sample data
+
+// Step 4: Solve Sample Data
+
+// Step 5: Function Signature
+
+// Step 6: List code constructs
+
+// Step 7: Pseudo Code
+
+// Step 8: Solve
